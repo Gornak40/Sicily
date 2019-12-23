79a2df7bcdd45d2d374aba6905563850cd07a7c5
Alexander Gorbunov <s-kozelsk@yandex.ru>
2019-12-23 13:23:27 +0300

    Process commit
    Add cards chosen for playing with bot

--- scr/main.py
+++ scr/main.py
@@ -1,15 +1,44 @@
 from engine import *
-from random import choice
+from random import choice, randrange
 from results import *
-enemy = CARDS.copy()
-me = CARDS.copy()
+take = lambda arr: arr.pop(randrange(len(arr)))
+
+redCards = CARDS.copy()
+blackCards = CARDS.copy()
+
+# traitors turn
+redTraitor = take(redCards)
+blackTraitor = take(blackCards)
+redCards.append(blackTraitor)
+blackCards.append(redTraitor)
+
+# making hands
+redHand = [take(redCards) for _ in range(3)]
+blackHand = [take(blackCards) for _ in range(3)]
 Game = Game()
+
+# game process
 while Game.redScore < 4 and Game.blackScore < 4:
-    Game.blackCard = choice(enemy)
-    Game.redCard = me[int(input())]
+    Game.blackCard = take(blackHand)
+    blackHand.append(take(blackCards)) if blackCards else None
+    print(redHand)
+    redNum = int(input())
+    ind = [i for i in range(len(redHand)) if redHand[i].power == redNum][0] 
+    Game.redCard = redHand.pop(ind)
+    redHand.append(take(redCards)) if redCards else None
     fight(Game)
     print('Scores:', Game.redScore, Game.blackScore)
     print('HoldScore:', Game.holdScore)
     print('GangBoosts:', Game.redGangBoost, Game.blackGangBoost)
     print('Boosts:', Game.redBoost, Game.blackBoost)
     print()
+
+# results
+print('{} PLAYER IS THE WINNER'.format('RED' if Game.redScore > Game.blackScore else 'BLACK'))
+
+
+
+
+
+
+
