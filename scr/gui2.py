aa099d6a60e8c3f4e59f1f7a633f8c505496dcc1
Alexander Gorbunov <s-kozelsk@yandex.ru>
2020-01-05 12:47:02 +0300

    GUI gameplay
    First GUI playable mod (except Gang Boost)

--- scr/gui2.py
+++ scr/gui2.py
@@ -2,28 +2,45 @@ import pygame as pg
 from cards import *
 from random import choice, randrange
 from results import *
+from engine import *
 
 
 def loadImage(name):
     return pg.transform.rotozoom(pg.image.load('../data/' + name), 0, 0.6)
 
 
-def render():
+def render(blackOpen):
     screen.fill(WHITE)
     for i in range(3):
         screen.blit(IMG[redHand[i].name], redRects[i])
-    screen.blit(IMG[Game.blackCard.name], blackRect)
+    screen.blit(image if not blackOpen else IMG[Game.blackCard.name], blackRect)
     pg.display.flip()
     pg.display.update()
     clock.tick(FPS)
 
 
 def update(event):
+    global blackOpen
     if event.type == pg.MOUSEBUTTONDOWN:
+        if blackOpen:
+            blackOpen = False
+            return
         pos = event.pos
+        card = None
         for i in range(3):
             if redRects[i].collidepoint(pos):
-                print(redHand[i])
+                card = redHand[i]
+# game process        
+        if card is not None:
+            blackOpen = True
+            Game.blackCard = take(blackHand)
+            blackHand.append(take(blackCards)) if blackCards else None
+            Game.redCard = redHand.pop(redHand.index(card))
+            redHand.append(take(redCards)) if redCards else None
+            fight(Game)
+            print('Scores:', Game.redScore, Game.blackScore)
+            print('HoldScore:', Game.holdScore)
+            print()
 
 
 # const
@@ -74,12 +91,15 @@ pg.display.set_caption('Sicily')
 pg.display.set_icon(pg.image.load('../data/icon.png'))
 
 # main loop
+blackOpen = False
 while running:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
-        card = update(event)
-    
-    render()
+        update(event)
+    render(blackOpen)
+    if Game.redScore >= 4 or Game.blackScore >= 4:
+        print('{} PLAYER IS THE WINNER'.format('RED' if Game.redScore > Game.blackScore else 'BLACK'))
+        running = False
 
 pg.quit()
